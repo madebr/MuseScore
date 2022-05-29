589de3ddec91331ded2b9c67b997855464106277
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-05-29 20:10:47 +0200

    Add BUILD_THIRDPARTY option that controls use of vendored libraries

--- build/cmake/SetupFreetype.cmake
+++ build/cmake/SetupFreetype.cmake
@@ -3,7 +3,7 @@
 ## freetype2 >= 2.5.2
 ##
 
-if (USE_SYSTEM_FREETYPE)
+if (NOT BUILD_THIRDPARTY)
       if (OS_IS_MAC)
             PKGCONFIG1 (freetype2 2.5.2 FREETYPE_INCLUDE_DIRS FREETYPE_LIBDIR FREETYPE_LIBRARIES FREETYPE_CPP)
             if (FREETYPE_INCLUDE_DIRS)
@@ -16,15 +16,15 @@ if (USE_SYSTEM_FREETYPE)
       else (OS_IS_MAC)
             find_package(Freetype REQUIRED)
       endif (OS_IS_MAC)
-endif (USE_SYSTEM_FREETYPE)
+endif (NOT BUILD_THIRDPARTY)
 
 
-if (USE_SYSTEM_FREETYPE)
+if (NOT BUILD_THIRDPARTY)
       include_directories(${FREETYPE_INCLUDE_DIRS})
-else (USE_SYSTEM_FREETYPE)
+else (NOT BUILD_THIRDPARTY)
       include_directories(${PROJECT_SOURCE_DIR}/thirdparty/freetype/include)
-endif (USE_SYSTEM_FREETYPE)
+endif (NOT BUILD_THIRDPARTY)
 
-if (NOT USE_SYSTEM_FREETYPE)
+if (BUILD_THIRDPARTY)
       add_subdirectory(thirdparty/freetype)
-endif (NOT USE_SYSTEM_FREETYPE)
+endif (BUILD_THIRDPARTY)
